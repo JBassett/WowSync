aaa77c3bc07eb83772c6fe1c3cc32c5feb7f6be6
Justin Bassett <Bassett.JustinT@gmail.com>
2015-08-12 11:28:18 -0400

    Initial commit.

---
+++ src/main/java/com/ssvnormandy/wowsync/Application.java
@@ -0,0 +1,55 @@
+package com.ssvnormandy.wowsync;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+/**
+ * Created by jbassett on 8/11/15.
+ */
+public class Application {
+
+    public static void main(String[] args){
+        Arguments arguments = new Arguments();
+        CmdLineParser cmdLineParser = new CmdLineParser(arguments);
+        try{
+            cmdLineParser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            cmdLineParser.printUsage(System.err);
+            System.exit(-1);
+        }
+
+        LinkCreator lc = new LinkCreator();
+        lc.createLinks(Paths.get(arguments.syncDir.toString(), "SavedVariables").toFile(),
+                Paths.get(arguments.wowDir.toString(), "WTF", "Account", arguments.accountName, "SavedVariables").toFile());
+    }
+
+    static class Arguments {
+
+        public Arguments() {
+            String baseWow;
+            if(System.getProperty("os.name").startsWith("Windows")) {
+                baseWow = System.getenv("ProgramFiles(X86)");
+            }
+            else {
+                baseWow = "/Applications/";
+            }
+            syncDir = Paths.get(System.getProperty("user.home"), "Dropbox", "Game Data", "World of Warcraft").toFile();
+            wowDir = Paths.get(baseWow, "World of Warcraft").toFile();
+
+        }
+
+        @Option(name="-s", aliases = {"--sync-dir"}, usage = "Sets the folder that is synced to your cloud storage.")
+        public File syncDir;
+
+        @Option(name="-w", aliases = {"--wow-dir"}, usage = "Sets the install location of your World of Warcraft Client.")
+        public File wowDir;
+
+        @Option(name = "-n", aliases = {"--name"}, usage = "The account name that you want to sync.")
+        public String accountName;
+    }
+}
---
+++ src/main/java/com/ssvnormandy/wowsync/LinkCreator.java
@@ -0,0 +1,62 @@
+package com.ssvnormandy.wowsync;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by jbassett on 8/12/15.
+ */
+public class LinkCreator {
+    public void createLinks(File src, File dest) {
+        if (src == null || !src.isDirectory()) {
+            throw new IllegalArgumentException("src is not a directory.");
+        }
+        if (dest == null) {
+            throw new IllegalArgumentException("dest cannot be null or not writeable.");
+        } else if (!dest.exists()) {
+            try {
+                Files.createDirectories(dest.toPath());
+            } catch (IOException e) {
+                throw new IllegalArgumentException("Could not create the destination directory.", e);
+            }
+        }
+
+        cleanupBadSymbolicLinks(dest);
+
+        Path link, source;
+        for (File file : src.listFiles(p -> p.isFile() && p.getName().endsWith("lua"))) {
+            link = Paths.get(dest.toPath().toString(), file.getName());
+            source = file.toPath();
+            if (Files.isSymbolicLink(link)) {
+                System.out.printf("File (%s) is already symbolic link, not doing anything.\n", link);
+                continue;
+            }
+            if (Files.exists(link)) {
+                System.out.printf("File (%s) is already in your folder, not doing anything.\n", link);
+                continue;
+            }
+            System.out.printf("Attempting to link source file (%s) to destination link (%s)\n", source, link);
+            try {
+                Files.createSymbolicLink(link, source);
+            } catch (IOException e) {
+                System.err.println("Issue linking!");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void cleanupBadSymbolicLinks(File loc) {
+        for (File file : loc.listFiles(pathname -> Files.isSymbolicLink(pathname.toPath()) && Files.notExists(pathname.toPath()))) {
+            System.out.printf("Deleting a symlink (%s) that no longer exists.", file);
+            try {
+                Files.delete(file.toPath());
+            } catch (IOException e) {
+                System.err.println("Could not delete the bad symlink...");
+                e.printStackTrace();
+            }
+        }
+    }
+}
